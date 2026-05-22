c42a7c3ffe7c60eadc6a7b49dfd2177603a42451
edssonzapata90 <edssonzapata99@gmail.com>
2026-05-22 13:38:45 -0500

    Update database.py

--- SoccerTrackerBackend/database.py
+++ SoccerTrackerBackend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./soccer_tracker.db"
+DATABASE_URL = "postgresql:///./soccer_tracker.db"
 
 engine = create_engine(
     DATABASE_URL,
