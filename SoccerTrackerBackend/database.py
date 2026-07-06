fad9e5691d3651b3602355d3a932563bf3a71f75
edssonzapata90 <edssonzapata99@gmail.com>
2026-07-06 14:19:41 -0500

    Update database.py

--- SoccerTrackerBackend/database.py
+++ SoccerTrackerBackend/database.py
@@ -1,12 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql:///./soccer_tracker.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL is None:
+    DATABASE_URL = "sqlite:///./soccer_tracker.db"
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
